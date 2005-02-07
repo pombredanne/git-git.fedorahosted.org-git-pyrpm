094802c317dbb962ca9a575715aefda338a23bb5
twoerner <twoerner>
2005-02-07 17:13:06 +0000

    [tw] hashlist  - using hashes for quick searches and lists for simple access

---
+++ hashlist.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Library General Public License as published by
+# the Free Software Foundation; version 2 only
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# Copyright 2004 Red Hat, Inc.
+#
+# Author: Thomas Woerner <twoerner@redhat.com>
+#
+
+import types
+
+class HashList:
+    """ hash list """
+
+    def __init__(self):
+        self.clear()
+
+    def clear(self):
+        self.list = [ ]
+        self.hash = { }
+
+    def __len__(self):
+        return len(self.list)
+
+    def __repr__(self):
+        return self.list.__repr__()
+    
+    def __getitem__(self, key):
+        if isinstance(key, types.IntType):
+            return (self.list[key], self.hash[self.list[key]])
+        if self.hash.has_key(key):
+            return self.hash[key] # return value only
+#        raise IndexError, "Indexing error"
+        
+    def __setitem__(self, key, value):
+        if not self.hash.has_key(key):
+            self.hash[key] = value
+            self.list.append(key)
+        else:
+            self.hash[key] = value
+        return self.hash[key]
+
+    def __delitem__(self, key):
+        if self.hash.has_key(key):
+            del self.hash[key]
+            self.list.remove(key)
+            return key
+        return None
+
+    def __contains__(self, key):
+        return self[key]
+
+    def has_key(self, key):
+        return (self[key] != None)
+        
+    def pop(self, idx):
+        key = self.list[idx]
+        self.list.pop(idx)
+        del self[key]
