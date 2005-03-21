d19a0a9f6a13dbecdfe438c98334f4fb964b04c9
harald <harald>
2005-03-21 12:57:54 +0000

    simple yum config file parser

---
+++ pyrpm/yumconfig.py
@@ -0,0 +1,266 @@
+#!python
+# -*- python -*-
+# -*- coding: utf-8 -*-
+## Copyright (C) 2005 Red Hat, Inc.
+## Copyright (C) 2005 Harald Hoyer <harald@redhat.com>
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+"""Simple yum.conf parser (read only)
+mostly copied from rhpl.ConfSMB
+"""
+
+
+from rhpl.Conf import Conf
+from glob import glob
+
+import sys
+from string import *
+import re
+import os
+from types import DictType
+
+class YumConfSubDict(DictType):
+    def __init__(self, parent_conf, stanza, initdict=None):
+        DictType.__init__(self, initdict)
+        self.conf = parent_conf
+        self.stanza = stanza
+        
+class YumConf(Conf):
+    """Simple Yum config file parser
+    """
+
+    MainVarnames = ( "cachedir",
+                     "reposdir",
+                     "debuglevel",
+                     "errorlevel",
+                     "logfile",
+                     "gpgcheck",
+                     "assumeyes",
+                     "tolerant",
+                     "exclude",
+                     "exactarch",
+                     "installonlypkgs",
+                     "kernelpkgnames",
+                     "showdupesfromrepos",
+                     "obsoletes",
+                     "overwrite_groups",
+                     "installroot",
+                     "rss-filename",
+                     "distroverpkg",
+                     "diskspacecheck",
+                     "tsflags",
+                     "recent",
+                     "retries",
+                     "keepalive",
+                     "throttle",
+                     "bandwidth",
+                     "commands",
+                     "proxy",
+                     "proxy_username",
+                     "proxy_password",
+                     # undocumented
+                     "pkgpolicy",
+                     )
+
+    RepoVarnames = ( "name",
+                     "baseurl",
+                     "mirrorlist",
+                     "enabled",
+                     "gpgcheck",
+                     "gpgkey",
+                     "exclude",
+                     "includepkgs",
+                     "enablegroups",
+                     "failovermethod",
+                     "keepalive",
+                     "retries",
+                     "throttle",
+                     "bandwidth",
+                     "proxy",
+                     "proxy_username",
+                     "proxy_password" )    
+
+    Variables = ( "releasever", "arch", "basearch" )
+    
+    def __init__(self, releasever, arch, basearch):
+        """releasever - version of release (e.g. 3 for Fedora Core 3)
+        arch - architecure (e.g. i686)
+        basearch - base architecture (e.g. i386)
+        """
+        self.releasever = releasever
+        self.arch = arch
+        self.basearch = basearch
+
+        self.stanza_re = re.compile('^\s*\[(?P<stanza>[^\]]*)]\s*(?:;.*)?$', re.I)
+        Conf.__init__(self, "/etc/yum.conf", '#;', '=', '=',
+                      merge=1, create_if_missing = 0)
+
+    def extendValue(self, value):
+        """replaces known $vars in values"""
+        for var in YumConf.Variables:
+            if value.find("$" + var) != -1:
+                value = value.replace("$" + var, self.__dict__[var])
+        return value
+
+    def checkVar(self, stanza, varname):
+        """check variablename, if allowed in the config file"""        
+        if stanza == "main":
+            if varname in YumConf.MainVarnames:
+                return 0
+        else:
+            if varname in YumConf.RepoVarnames:
+                return 0
+        return 1
+        
+    def read(self):
+        """read all config files"""
+        self.vars = {}
+        self.filename = '/etc/yum.conf'
+        Conf.read(self)
+        self.parseFile()
+        repodir = '/etc/yum.repos.d'
+        if self.vars.has_key("main") and self.vars["main"].has_key("reposdir"):
+            repodir = self.vars["main"]["reposdir"]
+            
+        filenames = glob(repodir + '/*.repo')
+        for filename in filenames:
+            self.filename = filename
+            Conf.read(self)
+            self.parseFile()    
+        
+    def parseFile(self):
+        """parse one config file with the help of Conf"""
+        self.rewind()
+        stanza = None
+        while 1:
+            stanza = self.next_stanza()
+            if not stanza:
+                break
+            stanzavars = {}
+            
+            self.nextline()
+            
+            while self.findnextcodeline():
+                vars = self.next_entry()
+                if not vars:
+                    break
+
+                if self.checkVar(stanza, vars[0]):
+                    raise Exception, "Bad variable %s" % vars[0]
+                
+                name = vars[0]
+                value = self.extendValue(vars[1])
+            
+                stanzavars[name] = value
+                self.nextline()
+
+            self.vars[stanza] = YumConfSubDict(self, stanza, stanzavars)
+            
+        self.rewind()
+
+    def get_entry(self):
+        vars = self.getfields()
+            
+        try:            
+            vars = [vars[0], joinfields(vars[1:len(vars)], '=')]
+        except(LookupError):
+            return 0
+
+        if not vars:
+            return 0
+      
+        return [strip(vars[0]), strip(vars[1])]
+
+    def next_entry(self):
+        while self.findnextcodeline():
+            #print "next_entry: " + self.getline()
+            if self.is_stanza_decl():
+                return 0
+            
+            vars = self.get_entry()
+            
+            if vars:
+                return vars
+            
+            self.nextline()            
+            
+        return 0
+                
+    def findnextcodeline(self):
+        return self.findnextline('^[\t ]*[\[A-Za-z_]+.*')
+    
+    def is_stanza_decl(self):
+        # return true if the current line is of the form [...]
+        if self.stanza_re.match(self.getline()):
+            return 1
+        return 0
+
+    def find_stanza(self, stanza_name):
+        # leave the current line at the first line of the stanza
+        # (the first line after the [stanza_name] entry)
+        self.rewind()
+        while self.findnextline('^[\t ]*\[.*\]'):
+            m = self.stanza_re.match(self.getline())
+
+            if m and (stanza_name == m.group('stanza')):
+                self.nextline()
+                return 1
+            
+            self.nextline()
+            
+        self.rewind()
+        return 0
+                
+    def next_stanza(self):
+        # leave the current line at the first line of the stanza
+        # (the first line after the [stanza_name] entry)
+        while self.findnextline('^[\t ]*\[.*\]'):
+            m = self.stanza_re.match(self.getline())
+            if m:
+                stanza = m.group('stanza')
+                if stanza:
+                    return stanza
+                
+            self.nextline()
+            
+        self.rewind()
+        return 0
+                
+    def prevline(self):
+        self.line = max([self.line - 1, 0])
+        
+    def __getitem__(self, stanza):               
+        return self.vars[stanza]
+        
+    def __setitem__(self, stanza, value):
+        raise Exception, "read only"
+            
+    def __delitem__(self, stanza):
+        raise Exception, "read only"
+            
+    def keys(self):
+        # no need to return list in order here, I think.
+        return self.vars.keys()
+    
+    def has_key(self, key):
+        return self.vars.has_key(key)
+
+    
+if __name__ == '__main__':
+    conf = YumConf("3", "i686", "i386")
+    print conf.vars
+    for confkey in conf.vars.keys():
+        print "key:", confkey
