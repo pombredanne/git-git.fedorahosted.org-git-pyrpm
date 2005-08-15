33288bdbd4bdd4bea51acee407a77cbd28b5ca27
laroche <laroche>
2005-08-15 10:10:55 +0000

    - 0.29 release

--- pyrpm/__init__.py
+++ pyrpm/__init__.py
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
 
-__version__ = "0.28"
+__version__ = "0.29"
 __doc__ = """Read and manage RPM packages."""
 
 import os, locale, sys
