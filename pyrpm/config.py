008330459933e1a41fc1a20173e1239945b45e02
pknirsch <pknirsch>
2005-09-21 12:43:37 +0000

    - Added the fileverifyflags for resolvertags for proper file conflict checks

--- pyrpm/config.py
+++ pyrpm/config.py
@@ -75,10 +75,9 @@ class RpmConfig:
             "requireflags", "requireversion", "obsoletename", "obsoleteflags",
             "obsoleteversion", "conflictname", "conflictflags",
             "conflictversion", "filesizes", "filemodes", "filemd5s",
-            "filelinktos", "fileflags", "filecolors", "dirindexes",
-            "basenames",
-            "dirnames", "oldfilenames", "sourcerpm", "md5",
-            "sha1header") # Tags used by RpmResolver
+            "filelinktos", "fileflags", "filecolors", "fileverifyflags",
+            "dirindexes", "basenames", "dirnames", "oldfilenames", "sourcerpm",
+            "md5", "sha1header") # Tags used by RpmResolver
         self.timer = 0                  # Output timing information
         self.ldconfig = 0             # Number of ldconfig calls optimized away
         self.delayldconfig = 0          # A delayed ldconfig call is pending
