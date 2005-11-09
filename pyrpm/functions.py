70d038092076ea7a0b8d1159c6b2179419b703b4
pknirsch <pknirsch>
2005-11-09 11:25:28 +0000

    - Dropped wrong raise in runScript() - Reworked the printError() usage in runScript()

--- pyrpm/functions.py
+++ pyrpm/functions.py
@@ -243,23 +243,22 @@ def runScript(prog=None, script=None, otherargs=[], force=False, rusage=False, t
         os.unlink(tmpfilename)
     if status != 0: #or cret != "":
         if os.WIFEXITED(status):
-            printError("Script %s ended with exit code %d:" % (str(args),
-                os.WEXITSTATUS(status)))
+            rpmconfig.printError("Script %s ended with exit code %d:" % \
+                                 (str(args), os.WEXITSTATUS(status)))
         elif os.WIFSIGNALED(status):
             core = ""
             if os.WCOREDUMP(status):
                 core = "(with coredump)"
-            printError("Script %s killed by signal %d%s:" % (str(args),
-                os.WTERMSIG(status), core))
+            rpmconfig.printError("Script %s killed by signal %d%s:" % \
+                                 (str(args), os.WTERMSIG(status), core))
         elif os.WIFSTOPPED(status):     # Can't happen, needs os.WUNTRACED
-            printError("Script %s stopped with signal %d:" % (str(args),
-                os.WSTOPSIG(status)))
+            rpmconfig.printError("Script %s stopped with signal %d:" % \
+                                 (str(args), os.WSTOPSIG(status)))
         else:
-            printError("Script %s ended (fixme: reason unknown):" % str(args))
-        if cret.endswith("\n"):
-            cret = cret[:-1]
-        printError(cret)
-        raise OSError, "Script %s failed" % (args,)
+            rpmconfig.printError("Script %s ended (fixme: reason unknown):" % \
+                                 str(args))
+        cret.rstrip()
+        rpmconfig.printError("Script %s failed: %s" % (args, cret))
     # FIXME: should we be swallowing the script output?
     return rusage_val
 
