abd72b1b7e94305f90a54899d633718cefb9bafd
laroche <laroche>
2006-09-29 05:13:00 +0000

    - release 0.60

--- scripts/oldpyrpm.py
+++ scripts/oldpyrpm.py
@@ -102,7 +102,7 @@
 # - "rhnplatform" could go away if it is not required.
 #
 
-__version__ = "0.59"
+__version__ = "0.60"
 __doc__ = """Manage everything around Linux RPM packages."""
 
 import sys
